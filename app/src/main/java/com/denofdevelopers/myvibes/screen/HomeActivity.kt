61d086285adf801fec7c142522a4d8c0d164d889
Viktor Jovanovski <BuktopMKD@gmail.com>
2020-03-15 20:17:05 +0100

    Rename MainActivity to HomeActivity

---
+++ app/src/main/java/com/denofdevelopers/myvibes/screen/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.denofdevelopers.myvibes.screen
+
+import android.os.Bundle
+import com.denofdevelopers.myvibes.R
+import com.denofdevelopers.myvibes.common.BaseActivity
+
+class HomeActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
